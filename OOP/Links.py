9acb7475a636e32a0c6d09cfb4603d052988036d
Nirotonin <131301125+Nirotonin@users.noreply.github.com>
2024-06-20 17:41:26 +0200

    Update Links.py

--- OOP/Links.py
+++ OOP/Links.py
@@ -6,15 +6,10 @@ import json
 def extract_links_from_page(url, save_folder):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    links = [a.get('href') for a in soup.find_all('a', href=True)]
+    links = [{'name': a.text.strip(), 'link': a.get('href')} for a in soup.find_all('a', href=True)]
 
-    save_path = os.path.join(save_folder, 'Links_url.json')
+    save_path = os.path.join(save_folder, 'Links.json')
     with open(save_path, 'w', encoding='utf-8') as file:
         json.dump(links, file, ensure_ascii=False, indent=4)
 
     print(f'Ссылки сохранены в {save_path}')
-
-# Пример использования, который можно удалить
-# url = "http://example.com"
-# save_folder = "/path/to/save"
-# extract_links_from_page(url, save_folder)
